ddbdb1c9ed64dd7b6fb21e519517c888602a4212
yeebinrong <yeebinrong@gmail.com>
2020-11-23 16:55:18 +0800

    bug fix

--- main.js
+++ main.js
@@ -17,13 +17,45 @@ const app = express();
 
 // Add CORS header to the response
 app.use(cors());
+app.use(express.json());
 
 // create resource
+// PUT /cart
+app.put('/cart/:id', (req, resp) => {
+    const id = req.params['id'];
+    const payload = req.body;
+    console.info(payload);
+    const idx = cart.findIndex(i => i.id == payload.id);
+    console.info(idx);
+    if (idx < 0) {
+        cart.push(payload);
+    } else {
+        cart[idx] = payload;
+    }
+
+    resp.status(200);
+    resp.type('application/json')
+    resp.send({});  
+})
+
 // GET /cart
 app.get('/cart', (req, resp) => {
     resp.status(200);
     resp.type('application/json');
     resp.json(cart);
+})
+
+app.get('/cart/:id', (req, resp) => {
+    let id = req.params.id;
+    let cartItem = cart.find(i => i.id == id)
+    resp.type('application/json')
+    if (cartItem === undefined) {
+        resp.status(404);
+        resp.json({});
+        return;
+    }
+    resp.status(200);
+    resp.json(cartItem);
 })
 
 // listen port
